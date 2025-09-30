d4aa16eccdcb445e7da2507fc515a17a6bacb815
czearing <czearing@outlook.com>
2025-09-30 16:16:49 -0700

    Fixing subscribe hook

--- utils/useSubscribe.ts
+++ utils/useSubscribe.ts
@@ -1,30 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-
-interface SubscribeData {
-  email: string;
-}
-
-interface SubscribeResponse {
-  success: boolean;
-  message?: string;
-}
-
-async function subscribeEmail(data: SubscribeData): Promise<SubscribeResponse> {
-  const response = await fetch("/api/subscribe", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-
-  if (!response.ok) {
-    const error = await response.json();
-    throw new Error(error.message || "Failed to subscribe");
-  }
-
-  return response.json();
-}
+import { subscribeEmail } from "./api/subscribe";
 
 export function useSubscribe() {
   return useMutation({
